15c1085437c843153cd0b064da591da30df1f0f9
reuschj <reuschj@gmail.com>
2020-06-10 01:05:34 -0500

    Initial commit

--- Tests/StrokedShapeTests/StrokedShapeTests.swift
+++ Tests/StrokedShapeTests/StrokedShapeTests.swift
@@ -6,7 +6,7 @@ final class StrokedShapeTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(StrokedShape().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 
     static var allTests = [
